65a12cfc5575ccbca7c7c54f1f8f2433dfa007bc
Ken Browning <ken.browning@hautelook.com>
2014-02-09 13:06:58 -0800

    Extract DocumentLines concern

--- lib/LineEmitter.ts
+++ lib/LineEmitter.ts
@@ -3,13 +3,31 @@ import ILine = require('./ILine');
 import INewlineFinder = require('./INewlineFinder');
 import ILineCallback = require('./ILineCallback');
 
+class DocumentLines {
+
+	private lineNumber = 1;
+	private offset = 0;
+
+	public createLine(text: string, newline: string) {
+		var line: ILine = {
+			number: this.lineNumber,
+			text: text,
+			newline: newline,
+			offset: this.offset
+		};
+		this.lineNumber++;
+		this.offset += text.length + newline.length;
+		return line;
+	}
+
+}
+
 class LineEmitter {
 
 	private newlineFinder: INewlineFinder;
 	private callback: ILineCallback;
+	private documentLines = new DocumentLines();
 	private buffer = '';
-	private lineNumber = 0;
-	private offset = 0;
 
 	constructor(newlineFinder: INewlineFinder, callback: ILineCallback) {
 		this.newlineFinder = newlineFinder;
@@ -21,30 +39,20 @@ class LineEmitter {
 		var offset = 0;
 		this.newlineFinder.find(this.buffer).forEach((newline) => {
 			var text = this.buffer.substring(offset, newline.index);
-			this.emitLine(this.createLine(text, newline.text));
+			this.emitLine(text, newline.text);
 			offset += text.length + newline.text.length;
 		});
 		this.buffer = this.buffer.substr(offset);
 	}
 
-	private createLine(text: string, newline: string) {
-		var line: ILine = {
-			number: ++this.lineNumber,
-			text: text,
-			newline: newline,
-			offset: this.offset
-		};
-		this.offset += text.length + newline.length;
-		return line;
-	}
-
-	private emitLine(line: ILine) {
+	private emitLine(text: string,  newline: string) {
+		var line = this.documentLines.createLine(text, newline);
 		this.callback(null, line);
 	}
 
 	public flushLines() {
 		if (this.buffer) {
-			this.emitLine(this.createLine(this.buffer, ''));
+			this.emitLine(this.buffer, '');
 		}
 	}
 }
