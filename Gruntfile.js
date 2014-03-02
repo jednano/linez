85c7807b5eba145c2c0e5e7799097a1684f5e6e9
Jed Mao <jed.hunsaker@gmail.com>
2014-03-01 21:51:15 -0600

    Bring back Gruntfile, ditch node solution

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+﻿module.exports = function(grunt) {
+
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: [
+			'lib/**/*.js',
+			'!lib/api.js',
+			'test/**/*.js'
+		],
+		typescript: {
+			options: {
+				module: 'commonjs',
+				target: 'es5'
+			},
+			test: {
+				src: 'test/**/*.ts',
+				dest: ''
+			}
+		},
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					clearRequireCache: true
+				},
+				src: 'test/**/*.js'
+			}
+		},
+		watch: {
+			ts: {
+				files: [
+					'test/**/*.ts'
+				],
+				tasks: ['typescript']
+			},
+			js: {
+				files: [
+					'lib/api.js',
+					'lib/**/*.ts',
+					'test/**/*.js'
+				],
+				tasks: ['mochaTest']
+			}
+		} 
+	});
+
+	require('load-grunt-tasks')(grunt);
+
+	// Default task(s).
+	grunt.registerTask('default', ['build', 'mochaTest', 'watch']);
+	grunt.registerTask('test', ['build', 'mochaTest', 'clean']);
+	grunt.registerTask('build', ['clean', 'typescript']);
+
+};
