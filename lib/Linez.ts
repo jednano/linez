849523e38fde71baa55217a5aee9993accd4ab8f
Ken Browning <ken.browning@hautelook.com>
2014-03-06 10:11:16 -0800

    wip

--- lib/Linez.ts
+++ lib/Linez.ts
@@ -1,11 +1,10 @@
 ﻿import ILine = require('./ILine');
 
+class StringFinder {
 
-class Linez {
+	private _newlinesExpression;
 
-	private _newlinesExpression = /\r?\n/g;
-
-	public set newlines(newlines: string[]) {
+	constructor(newlines: string[]) {
 		this._newlinesExpression = this.convertToPipedExpression(newlines);
 	}
 
@@ -17,21 +16,46 @@ class Linez {
 		return result;
 	}
 
+	public findAll(text) {
+		var matches = [];
+		var match;
+		while(match = this._newlinesExpression.exec(text)) {
+			matches.push({
+				index: match.index,
+				text: match[0]
+			});
+		}
+		return matches;
+	}
+}
+
+
+class Linez {
+
+	private lineEndingFinder: StringFinder;
+
+	constructor() {
+		this.lineEndingFinder = new StringFinder(['\r\n', '\n']);
+	}
+
+	public set newlines(newlines: string[]) {
+		this.lineEndingFinder = new StringFinder(newlines);
+	}
+
 	parse(text: string) {
 		var lines: ILine[] = [];
 		var lineNumber = 1;
 		var lineOffset = 0;
-		text.replace(this._newlinesExpression, (match, offset) => {
+		this.lineEndingFinder.findAll(text).forEach(function(lineEnding) {
 			lines.push({
 				number: lineNumber++,
 				offset: lineOffset,
-				text: text.substring(lineOffset, offset),
-				ending: match
+				text: text.substring(lineOffset, lineEnding.index),
+				ending: lineEnding.text
 			});
-			lineOffset = offset + match.length;
-			return match;
+			lineOffset = lineEnding.index + lineEnding.text.length;
 		});
-		if (lineOffset !== text.length || text === '') {
+		if (lineOffset < text.length || text === '') {
 			lines.push({
 				number: lineNumber,
 				offset: lineOffset,
